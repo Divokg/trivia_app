a9720cd3d2e513d1a6afe859c670b37ea0d6d9f1
Samuel Ndungu <ndungusam1@gmail.com>
2022-08-31 16:42:12 +0300

    Update __init__.py

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -12,7 +12,7 @@ def paginate_questions(request, selection):
   start = (page-1) * QUESTIONS_PER_PAGE
   end = start + QUESTIONS_PER_PAGE
   
-  questions = [questions.format() for question in selection]
+  questions = [question.format() for question in selection]
   current_questions = questions[start:end]
   
   return current_questions
